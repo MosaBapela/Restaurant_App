5fbc172f107cb38ea8ec7117de6845074aa4fd84
MosaBapela <charitybapela2003@gmail.com>
2025-12-11 04:43:52 +0200

    Added mock data for testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
+import { AppNavigator } from './src/navigation/AppNavigator';
+import { store } from './src/redux/store';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <AppNavigator />
+        <StatusBar style="auto" />
+      </Provider>
+    </GestureHandlerRootView>
   );
 }
